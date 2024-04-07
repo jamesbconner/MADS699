1c799105efea861735fb4474ba65df02dba5056f
james conner <jamcon@umich.edu>
2024-04-07 11:47:42 -0400

    Creating model_score common function

--- notebooks/common_functions.py
+++ notebooks/common_functions.py
@@ -6,6 +6,12 @@ import numpy as np
 import env_functions as ef
 import s3_functions as sf
 
+# Import model libraries
+import xgboost as xgb
+import lightgbm as lgb
+from sklearn.linear_model import ElasticNet
+from sklearn.ensemble import RandomForestRegressor, HistGradientBoostingRegressor
+
 # Determine the environment and get appropriate vars
 deepnote, env_vars = ef.load_env_vars()
 
@@ -24,10 +30,13 @@ if not deepnote:
     }
 
 
-def import_data(dn_path="/work/data/Xy_Data", s3_path="data/Xy_Data", location_name='CARB'):
+def import_data(dn_path="/work/data/Xy_Data", s3_path="data/Xy_Data", location_name=None):
     """
     This function imports the data from the specified data path and location geography
     """
+
+    if location_name == None:
+        raise ValueError("'location_name' must be set")
 
     data_path = None
 
@@ -58,9 +67,6 @@ def import_data(dn_path="/work/data/Xy_Data", s3_path="data/Xy_Data", location_n
     y_holdout_trans = y_holdout
 
     return X_train_trans, X_val_trans, X_holdout_trans, y_train_trans, y_val_trans, y_holdout_trans
-
-# Import some data temporarily, as some of the other functions use the Xy data in argument defaults
-X_train_trans, X_val_trans, X_holdout_trans, y_train_trans, y_val_trans, y_holdout_trans = import_data(location_name='CARB')
 
 
 def plot_feat_importance(model):
@@ -117,7 +123,7 @@ def plot_feat_importance(model):
 
 
 def write_out(model, trials, params, feat_cols=[], dn_path="/work/models", dns_path="/datasets/s3/models",
-              s3_path="/models", model_family="xgboost_reg", location_name="CARB"):
+              s3_path="/models", model_family="xgboost_reg", location_name=None):
     """
     Write out the model artifacts to disk
 
@@ -128,6 +134,10 @@ def write_out(model, trials, params, feat_cols=[], dn_path="/work/models", dns_p
     model_family: The model family, e.g. 'xgboost_reg', 'lightgbm_reg'
     location_name: The geography that the model is trained for, e.g. 'SEAA', 'CARB', 'GLOB'
     """
+
+    if location_name == None:
+        raise ValueError("'location_name' argument must be set")
+
     date_time_str = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
     dn_model_path = os.path.join(dn_path, model_family, location_name)
     dns_model_path = os.path.join(dns_path, model_family, location_name)
@@ -175,3 +185,88 @@ def write_out(model, trials, params, feat_cols=[], dn_path="/work/models", dns_p
         # Write out the feature columns if they exist
         if len(feat_cols) > 0:
             write_to_s3(file_path=f"{s3_model_path}/{date_time_str}_feat_cols.pkl", data=feat_cols, pickle_file=True, **aws_env_vars)
+
+
+def model_score(hps, val=True, train=False, holdout=False, Xtt=None, ytt=None, Xvt=None, yvt=None, Xht=None, yht=None, model_type=None):
+    """
+    This function rebuilds the model with the desired hyperparameters
+    val: True will evaluate the model on the validation data
+    holdout: True will evaluate the model on the holdout data
+    train: True will evaluate the model on the train data
+    Xtt: X_train_trans
+    ytt: y_train_trans
+    Xvt: X_val_trans
+    yvt: y_val_trans
+    Xht: X_holdout_trans
+    yht: y_holdout_trans
+    model_type: The type of model to fit and score
+    """
+
+    if not all(x is not None for x in [Xtt, ytt, Xvt, yvt, Xht, yht]):
+        raise ValueError("Data is missing (all parameters for X and y data must be provided)")
+
+    if model_type == None:
+        raise ValueError("'model_type' argument must be set")
+
+    elif model_type == 'xgb':
+        model = xgb.XGBRegressor(**hps)
+
+    elif model_type == 'lgbm':
+        model = lgb.LGBMRegressor(**hps, verbose=-1, random_state=42, n_jobs=-1)
+
+    elif model_type == 'hgbm':
+        model = HistGradientBoostingRegressor(**hps)
+
+    elif model_type == 'rf':
+        model = RandomForestRegressor(**hps)
+
+    elif model_type == 'enet':
+        model = ElasticNet(**hps, random_state=42)
+
+    else:
+        raise ValueError(f"Unknown model_type: {model_type}")
+
+
+    model.fit(Xtt, ytt)
+
+    # Evaluate model and print results
+    if train:
+        # Evaluation on train data
+        train_pred = model.predict(Xtt)
+        train_mae = mean_absolute_error(ytt, train_pred)
+        train_mse = mean_squared_error(ytt, train_pred)
+        train_rmse = mean_squared_error(ytt, train_pred, squared=False)
+        train_rsq = r2_score(ytt, train_pred)
+        print(" ")
+        print(f"Train Mean Absolute Error: {train_mae:.4f}")
+        print(f"Train Mean Squared Error: {train_mse:.4f}")
+        print(f"Train Root Mean Squared Error: {train_rmse:.4f}")
+        print(f"Train R^2 Score: {train_rsq:.4f}")
+
+    if val:
+        # Evaluation on validation data
+        val_pred = model.predict(Xvt)
+        val_mae = mean_absolute_error(yvt, val_pred)
+        val_mse = mean_squared_error(yvt, val_pred)
+        val_rmse = mean_squared_error(yvt, val_pred, squared=False)
+        val_rsq = r2_score(yvt, val_pred)
+        print(" ")
+        print(f"Validation Mean Absolute Error: {val_mae:.4f}")
+        print(f"Validation Mean Squared Error: {val_mse:.4f}")
+        print(f"Validation Root Mean Squared Error: {val_rmse:.4f}")
+        print(f"Validation R^2 Score: {val_rsq:.4f}")
+
+    if holdout:
+        # Evaluation on holdout data
+        holdout_pred = model.predict(Xht)
+        holdout_mae = mean_absolute_error(yht, holdout_pred)
+        holdout_mse = mean_squared_error(yht, holdout_pred)
+        holdout_rmse = mean_squared_error(yht, holdout_pred, squared=False)
+        holdout_rsq = r2_score(yht, holdout_pred)
+        print(" ")
+        print(f"Holdout Mean Absolute Error: {holdout_mae:.4f}")
+        print(f"Holdout Mean Squared Error: {holdout_mse:.4f}")
+        print(f"Holdout Root Mean Squared Error: {holdout_rmse:.4f}")
+        print(f"Holdout R^2 Score: {holdout_rsq:.4f}")
+
+    return model
